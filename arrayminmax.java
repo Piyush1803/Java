8bc521946a9df9af712cb7b9c527e1dd13c9ace9
Piyush1803 <95519234+Piyush1803@users.noreply.github.com>
2022-03-31 14:04:49 +0530

    Add files via upload

---
+++ arrayminmax.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class arrayminmax {
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter array length ");
+        int n = sc.nextInt();
+        int X []=new int [n];
+        System.out.println("enter the elements ");
+        for (int i=0; i<n; i++)
+        {
+            X[i]= sc.nextInt();
+        }
+        sc.close(); 
+        System.out.println();
+        int minin =0;
+        int maxin=0;
+        for (int i=0; i<n; i++)
+        {
+            if (X[i]> X[maxin])
+                maxin=i;
+            if (X[i]< X[minin])
+                minin=i;
+
+        }
+        int maxval = X[maxin];
+        int minval = X[minin];
+
+        System.out.println("maximum value:"+ maxval);
+        System.out.println("minimum value:"+ minval);
+    }
+    
+}
