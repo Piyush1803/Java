d6d0857eb627dd4402e215ef671a1ee6f4ff89d1
Piyush1803 <95519234+Piyush1803@users.noreply.github.com>
2022-04-14 13:21:33 +0530

    Add files via upload

---
+++ rectpattern.java
@@ -0,0 +1,37 @@
+public class rectpattern {
+    public static void main(String[] args) {
+       
+
+        
+        
+        /*        Triangle Pattern  */
+        for (int rows = 0; rows < 5; rows++){
+            for (int j = 0; j < rows; j++){
+                System.out.print("*");
+            }
+        System.out.println();
+    }
+    
+ }
+    
+}
+
+/*    Rectangle Pattern 
+
+    for (int rows = 1; rows < 6; rows++) {
+        for (int cols = 1; cols < 5; cols++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+*/    
+
+/*          Sqaure Pattern
+
+    for (int rows = 1; rows < 6; rows++) {
+      for (int j = 1; j <=5; j++) {
+            System.out.print("*");
+    }
+    System.out.println();
+}
+*/
